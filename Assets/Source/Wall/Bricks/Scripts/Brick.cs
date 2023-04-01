0aa4fadba48d47d3b4783b6597490312f6d24129
Pavel Ol <megapiharr2008@gmail.com>
2023-04-01 13:11:56 +0300

    Added additiona test scene and particles to brick

--- Assets/Source/Wall/Bricks/Scripts/Brick.cs
+++ Assets/Source/Wall/Bricks/Scripts/Brick.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class Brick : MonoBehaviour
 {
-    
+    [SerializeField] private float rotationH = 500.5f;
+    [SerializeField] private float rotationV = 1000.5f;
+    [SerializeField] private float secondsToDestroy = 3f;
+    [SerializeField] private ParticleSystem _particles;
     private void Start()
     {
-        
+        // Remove in prod
+        AddRotation();
+        _particles.Play();
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -23,7 +28,17 @@ public class Brick : MonoBehaviour
     {
         if (other.GetComponent<PlayerCollisionHandler>())
         {
-            Destroy(gameObject);
+            Destroy(gameObject, secondsToDestroy);
+            AddRotation();
+            //Add particles with star, on hit
+            _particles.Play();
         }
+    }
+
+    private void AddRotation()
+    {
+        //GetComponent<Rigidbody>().useGravity = true;
+        GetComponent<Rigidbody>().AddTorque(transform.up * rotationH * rotationV * Random.Range(1.1f, 2f));
+        GetComponent<Rigidbody>().AddTorque(transform.right * rotationH * rotationV * Random.Range(1.1f, 2f));
     }
 }
